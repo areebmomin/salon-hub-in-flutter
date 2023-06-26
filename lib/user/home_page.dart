f8b86d245a9404aebdecfe5ce8eb57035c2287b5
Areeb Momin <mohammadareeb.m@peopleinteractive.in>
2023-06-26 20:24:04 +0530

    add home page

---
+++ lib/user/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SaloonHomePage extends StatelessWidget {
+  const SaloonHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
