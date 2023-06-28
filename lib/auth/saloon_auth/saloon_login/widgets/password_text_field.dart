1f76b256259359c9379c588e8d32a6c69d0a3e5c
Areeb Momin <mohammadareeb.m@peopleinteractive.in>
2023-06-28 17:30:31 +0530

    separate each widget in user login

---
+++ lib/auth/saloon_auth/saloon_login/widgets/password_text_field.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:otp_text_field/otp_field.dart';
+import 'package:otp_text_field/otp_field_style.dart';
+import 'package:otp_text_field/style.dart';
+
+import '../../../../utils/index.dart';
+
+class PasswordTextFieldWidget extends StatelessWidget {
+  final Function(bool isValid) onInputValidated;
+  static const int _otpLength = 6;
+
+  const PasswordTextFieldWidget(this.onInputValidated, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 19, top: 8),
+          child: Text(
+            Strings.passcode,
+            style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.w500,
+              color: AppColors.headingTextColor,
+            ),
+          ),
+        ),
+        Padding(
+          padding:
+          const EdgeInsets.only(left: 19, right: 19, top: 9, bottom: 12),
+          child: OTPTextField(
+            length: _otpLength,
+            fieldWidth: 56,
+            fieldStyle: FieldStyle.box,
+            outlineBorderRadius: 5,
+            otpFieldStyle: OtpFieldStyle(
+              backgroundColor: AppColors.inputFieldBackground,
+              borderColor: AppColors.inputFieldBackground,
+              enabledBorderColor: AppColors.inputFieldBackground,
+            ),
+            style: const TextStyle(
+              color: AppColors.inputText,
+              fontWeight: FontWeight.w500,
+              fontSize: 17,
+            ),
+            contentPadding:
+            const EdgeInsets.symmetric(vertical: 16, horizontal: 4),
+            onChanged: (code) {
+              onInputValidated(code.length == _otpLength);
+            },
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 18),
+          child: GestureDetector(
+            onTap: () {
+              Fluttertoast.showToast(msg: 'msg');
+            },
+            child: const Text(
+              Strings.forgotPasscode,
+              textAlign: TextAlign.end,
+              style: TextStyle(
+                fontSize: 16,
+                fontFamily: Strings.firaSans,
+                fontWeight: FontWeight.w500,
+                color: AppColors.headingTextColor,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
