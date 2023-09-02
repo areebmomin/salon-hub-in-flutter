b916e7b11f7ab247ec7cd25f906c4a9ee9bfdafd
Areeb Momin <mohammadareeb.m@peopleinteractive.in>
2023-09-02 18:51:33 +0530

    remove repeated button text style

--- lib/auth/saloon_auth/saloon_registration/business_details_page/widgets/save_details_button.dart
+++ lib/auth/saloon_auth/saloon_registration/business_details_page/widgets/save_details_button.dart
@@ -22,12 +22,7 @@ class SaveDetailsButton extends StatelessWidget {
         ),
         child: const Text(
           Strings.saveDetailsUpperCase,
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w700,
-            fontFamily: Strings.firaSans,
-            color: Colors.white,
-          ),
+          style: TextStyleConstants.buttonTextStyle,
         ),
       ),
     );
