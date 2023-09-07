7d659aa4da588d9f80b3f56495cea86ba57363fe
Areeb Momin <mohammadareeb.m@peopleinteractive.in>
2023-09-07 21:52:22 +0530

    optimise attendee details listview

--- lib/auth/saloon_auth/saloon_registration/business_details_page/widgets/attendee_details_section.dart
+++ lib/auth/saloon_auth/saloon_registration/business_details_page/widgets/attendee_details_section.dart
@@ -31,72 +31,10 @@ class AttendeeDetails extends StatelessWidget {
               return ListView.builder(
                 physics: const NeverScrollableScrollPhysics(),
                 shrinkWrap: true,
+                prototypeItem: AttendeeDetailsListItem(cubit, 0),
                 itemCount: cubit.data.attendeeDetailList.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(
-                      left: 20,
-                      right: 20,
-                      top: 12,
-                      bottom: 12,
-                    ),
-                    child: Row(
-                      children: [
-                        Stack(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8),
-                              child: BlocBuilder<SaloonRegistrationCubit,
-                                  SaloonRegistrationState>(
-                                buildWhen: (previousState, state) {
-                                  return state is AttendeePhotoSelected &&
-                                      state.index == index;
-                                },
-                                builder: (context, state) {
-                                  return CircleAvatar(
-                                    backgroundImage: _getBackgroundImage(state),
-                                    radius: 40,
-                                  );
-                                },
-                              ),
-                            ),
-                            Positioned(
-                              top: 70,
-                              left: 67,
-                              child: GestureDetector(
-                                onTap: () {
-                                  cubit.setAttendeePhoto(index);
-                                },
-                                child: const Icon(
-                                  Icons.add_a_photo,
-                                  size: 24,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(width: 16),
-                        Expanded(
-                          child: Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              TextField(
-                                decoration: const InputDecoration(
-                                  hintStyle: TextStyleConstants.textFieldHint,
-                                  hintText: Strings.name,
-                                ),
-                                onChanged: (name) {
-                                  cubit.data.attendeeDetailList[index].name =
-                                      name;
-                                },
-                              ),
-                              if (index > 0) _getCloseButton(cubit, index),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
+                  return AttendeeDetailsListItem(cubit, index);
                 },
               );
             },
@@ -118,6 +56,86 @@ class AttendeeDetails extends StatelessWidget {
       ),
     );
   }
+}
+
+class AttendeeDetailsListItem extends StatelessWidget {
+  final SaloonRegistrationCubit cubit;
+  final int index;
+
+  const AttendeeDetailsListItem(this.cubit, this.index, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 20,
+        right: 20,
+        top: 12,
+        bottom: 12,
+      ),
+      child: Row(
+        children: [
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: BlocBuilder<SaloonRegistrationCubit,
+                    SaloonRegistrationState>(
+                  buildWhen: (previousState, state) {
+                    return state is AttendeePhotoSelected &&
+                        state.index == index;
+                  },
+                  builder: (context, state) {
+                    return CircleAvatar(
+                      backgroundImage: _getBackgroundImage(state),
+                      radius: 40,
+                    );
+                  },
+                ),
+              ),
+              Positioned(
+                top: 70,
+                left: 67,
+                child: GestureDetector(
+                  onTap: () {
+                    cubit.setAttendeePhoto(index);
+                  },
+                  child: const Icon(
+                    Icons.add_a_photo,
+                    size: 24,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                TextField(
+                  decoration: const InputDecoration(
+                    hintStyle: TextStyleConstants.textFieldHint,
+                    hintText: Strings.name,
+                  ),
+                  onChanged: (name) {
+                    cubit.data.attendeeDetailList[index].name = name;
+                  },
+                ),
+                if (index > 0) _getCloseButton(cubit, index),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  ImageProvider<Object>? _getBackgroundImage(SaloonRegistrationState state) {
+    return state is AttendeePhotoSelected
+        ? FileImage(state.profilePicture) as ImageProvider<Object>?
+        : const AssetImage(Assets.userProfileDummy);
+  }
 
   Widget _getCloseButton(SaloonRegistrationCubit cubit, int index) {
     return SizedBox(
@@ -136,11 +154,5 @@ class AttendeeDetails extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  ImageProvider<Object>? _getBackgroundImage(SaloonRegistrationState state) {
-    return state is AttendeePhotoSelected
-        ? FileImage(state.profilePicture) as ImageProvider<Object>?
-        : const AssetImage(Assets.userProfileDummy);
   }
 }
